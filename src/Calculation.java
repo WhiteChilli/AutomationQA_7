1b7048676c1108b921ea79b6dafb772e93204ca1
WhiteChilli <locmerea@gmail.com>
2023-03-05 13:55:50 +0200

    Homework 7 corrected mistakes

--- src/Calculation.java
+++ src/Calculation.java
@@ -5,31 +5,33 @@ public class Calculation {
     public static void main(String[] args) {
         final int LENGTH = 7;
         int[] myArray;
-
+        myArray = new int[LENGTH];
         Random rd = new Random();
 
-        for (int i = 1; i < LENGTH; i++) {
-                System.out.println(rd.nextInt());
-            }
+        for (int i = 0; i < LENGTH; i++) {
+            myArray[i] = rd.nextInt();
+            System.out.println(myArray[i]);
+        }
+
 
         int[] tenNumArray;
-            tenNumArray = new int[10];
-            // for loop creates 1 t 10 and adds to the tenNumArray
-            for (int i = 0; i < tenNumArray.length; i++) {
-                tenNumArray[i] = i + 1;
-                System.out.println(tenNumArray[i]);
-            }
+        tenNumArray = new int[10];
+        // for loop creates 1 t 10 and adds to the tenNumArray
+        for (int i = 0; i < tenNumArray.length; i++) {
+            tenNumArray[i] = i + 1;
+            System.out.println(tenNumArray[i]);
+        }
 
         int[] evenArray;
-            evenArray = new int [5];
+        evenArray = new int[5];
 
         int index = 0; // counter for even array
-            for (int i = 0; i < tenNumArray.length; i++) {
-                if (tenNumArray[i] % 2 == 0) {
-                    evenArray[index] = tenNumArray[i];
-                    index++;
-                }
+        for (int i = 0; i < tenNumArray.length; i++) {
+            if (tenNumArray[i] % 2 == 0) {
+                evenArray[index] = tenNumArray[i];
+                index++;
             }
+        }
 
         int k = 0; // counter for while loop
         while (k < evenArray.length) {
@@ -37,7 +39,7 @@ public class Calculation {
             k++;
         }
 
-            }
+    }
 
 }
 
